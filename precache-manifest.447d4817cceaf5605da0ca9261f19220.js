9a73500e2ad1c65975b82f29826ddc73184fcd75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-26 05:14:23 +0000

    Deploy amarkantku/fontomezo to github.com/amarkantku/fontomezo.git:gh-pages

---
+++ precache-manifest.447d4817cceaf5605da0ca9261f19220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2877a9a1c01ae03a10e21b31071e97f4",
+    "url": "/fontomezo/index.html"
+  },
+  {
+    "revision": "7299abe370fa630e352a",
+    "url": "/fontomezo/static/css/main.11456861.chunk.css"
+  },
+  {
+    "revision": "d0850ff13a9f905e048f",
+    "url": "/fontomezo/static/js/2.30d5274d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fontomezo/static/js/2.30d5274d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7299abe370fa630e352a",
+    "url": "/fontomezo/static/js/main.a991aee6.chunk.js"
+  },
+  {
+    "revision": "cd8d8c9f42bd86b1aa41",
+    "url": "/fontomezo/static/js/runtime-main.48f3eda7.js"
+  }
+]);
